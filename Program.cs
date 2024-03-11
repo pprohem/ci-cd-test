504e061f6f16b76f026003d219b1978265c63e6d
pprohem <pprg00@gmail.com>
2024-03-11 01:00:16 -0300

    feat: Commit inicial. criando classe basica e classe de testes.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿
+using System;
+
+
+class Program
+{
+    
+
+    // Código escrito apenas para testes de Integração e Deploy Continuo. 
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Operacoes");
+
+
+     
+        int resultadoAdicao = Adicao(5, 3);
+        Console.WriteLine($"5 + 3 = {resultadoAdicao}");
+
+     
+        int resultadoSubtracao = Subtracao(10, 7);
+        Console.WriteLine($"10 - 7 = {resultadoSubtracao}");
+
+       
+        int resultadoMultiplicacao = Multiplicacao(4, 6);
+        Console.WriteLine($"4 * 6 = {resultadoMultiplicacao}");
+
+     
+        double resultadoDivisao = Divisao(20, 5);
+        Console.WriteLine($"20 / 5 = {resultadoDivisao}");
+
+        Console.ReadLine();
+
+
+        
+    }
+    public static int Adicao(int a, int b)
+    {
+
+        return a + b;
+    }
+
+
+    public static int Subtracao(int a, int b)
+    {
+        return a - b;
+    }
+
+    public static int Multiplicacao(int a, int b)
+    {
+        return a * b;
+    }
+
+    public static int Divisao(int a, int b)
+    {
+        if (b == 0)
+            return 0;
+        else
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Tests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMathCICD
+{
+    internal class Tests
+    {
+        [Test]
+    public void TestAdicao()
+    {
+      Equals(8, Program.Adicao(5, 3));
+    }
+
+    [Test]
+    public void TestSubtracao()
+    {
+           Equals(3, Program.Subtracao(10, 7));
+    }
+
+    [Test]
+    public void TestMultiplicacao()
+    {
+           Equals(24, Program.Multiplicacao(4, 6));
+    }
+
+    [Test]
+    public void TestDivisao()
+    {
+            Equals(4, Program.Divisao(20, 5));
+    }
+
+    [Test]
+    public void TestDivisaoPorZero()
+    {
+            Equals(() => Program.Divisao(10, 0), Throws.TypeOf<System.DivideByZeroException>());
+    }
+    }
+}
